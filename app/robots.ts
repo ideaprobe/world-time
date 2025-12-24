e4d87fbb656d473997f1c7841c74ade4ff7e49f9
cola <0zero@cola.com>
2025-12-24 13:47:53 +0800

    refactor: 去除robots disallow

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
       },
     ],
     sitemap: `${siteUrl}/sitemap.xml`,
